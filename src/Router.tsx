59febb05b45b2897080f6b387f5c0b63d293e8bf
iziz9 <46iz.us2@gmail.com>
2023-09-13 20:34:29 +0900

    Chore: 라우터 설정 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import ChartPage from './pages/ChartPage';
 import ErrorPage from './pages/ErrorPage';
 
 const Router = () => {
 	return (
 		<Routes>
+			<Route path="/" element={<Navigate to="/chart" replace={true} />} />
 			<Route path="/chart" element={<ChartPage />} />
 			<Route path="/*" element={<ErrorPage />} />
 		</Routes>
